bfeb8fa3b8a39f6890431b84da7101a6a35c202a
TOG B <4ir@BTOG-MacBook-Pro.local>
2023-10-03 20:32:54 +0900

    feat:고객 정보 수정 추가

--- src/main/java/com/coloronme/admin/global/security/WebSecurityConfig.java
+++ src/main/java/com/coloronme/admin/global/security/WebSecurityConfig.java
@@ -34,7 +34,7 @@ public class WebSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
+/*    @Bean
     public WebSecurityCustomizer ignoringCustomizer() {
         return (web) -> web.ignoring()
                 .requestMatchers("/h2-console/**",
@@ -43,12 +43,13 @@ public class WebSecurityConfig {
                         "/configuration/ui",
                         "/configuration/security",
                         "/webjars/**");
-    }
+    }*/
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http.csrf((csrf) -> csrf.disable());
+        System.out.println("SecurityFilterChain-----------------------------");
+        http.csrf((csrf) -> csrf.disable())
+                        .httpBasic((httpBasic) -> httpBasic.disable());
                 http
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
