bf1a0dcfbc5897c9ffe733e13e87db94e47e558b
erinlee <gusdk1793@gmail.com>
2023-10-26 17:18:54 +0900

    chore:오류 해결

--- src/main/java/com/coloronme/admin/domain/personalColor/entity/PersonalColor.java
+++ src/main/java/com/coloronme/admin/domain/personalColor/entity/PersonalColor.java
@@ -1,12 +1,8 @@
 package com.coloronme.admin.domain.personalColor.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDateTime;
 
