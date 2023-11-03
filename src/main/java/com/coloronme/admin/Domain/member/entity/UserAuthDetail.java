1678d2356c7f464916bb20f92f7645b76dcafdb5
TOG B <4ir@BTOG-MacBook-Pro.local>
2023-11-03 20:43:38 +0900

    feature:UserAuthDetail Entity 추가

---
+++ src/main/java/com/coloronme/admin/Domain/member/entity/UserAuthDetail.java
@@ -0,0 +1,38 @@
+package com.coloronme.admin.Domain.member.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "`UserAuthDetail`")
+public class UserAuthDetail {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String uuid;
+    @Column(name="`uuidExpiredAt`")
+    private String uuidExpiredAt;
+    @Column(name="`currentHashedRefreshToken`")
+    private String currentHashedRefreshToken;
+    @Column(name="`agreedToTerms`")
+    private boolean agreedToTerms;
+    @Column(name="`agreedToPrivacy`")
+    private boolean agreedToPrivacy;
+    @Column(name="`isAgeRequirementAgree`")
+    private boolean isAgeRequirementAgree;
+    @Column(name="`createdAt`")
+    private LocalDateTime createdAt;
+    @Column(name="`updatedAt`")
+    private LocalDateTime updatedAt;
+    @Column(name="`deletedAt`")
+    private LocalDateTime deletedAt;
+}
