6f6f88bb2e5ed5c77a91152851403dfe4d079e46
TOG B <4ir@BTOG-MacBook-Pro.local>
2023-11-16 12:40:48 +0900

    ⭐Feat : ConsultService 수정

--- src/main/java/com/coloronme/admin/domain/consult/dto/response/ConsultUserResponseDto.java
+++ src/main/java/com/coloronme/admin/domain/consult/dto/response/ConsultUserResponseDto.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ConsultUserResponseDto {
+    private int memberId;
     private String nickname;
     private String email;
     private LocalDateTime personalDate;
