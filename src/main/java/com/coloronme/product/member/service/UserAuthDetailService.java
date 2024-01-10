39792f8e398eac939344cd9beaf988b7d4f93179
TOG B <4ir@BTOG-MacBook-Pro.local>
2024-01-10 09:10:43 +0900

    🍕Chore : UserDetailService 수정

--- src/main/java/com/coloronme/product/member/service/UserAuthDetailService.java
+++ src/main/java/com/coloronme/product/member/service/UserAuthDetailService.java
@@ -32,6 +32,12 @@ public class UserAuthDetailService {
 
         UserAuthDetail userAuthDetailData = userAuthDetail.get();
 
+        Optional<Member> member = memberRepository.findByUserAuthDetailId(userAuthDetailData.getId());
+
+        if(member.isEmpty()) {
+            throw new RequestException(ErrorCode.USER_NOT_FOUND_404);
+        }
+
         /*기간이 만료된 qr일 경우*/
         String userExpiredAtData = userAuthDetailData.getUuidExpiredAt();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
@@ -40,12 +46,6 @@ public class UserAuthDetailService {
 
         if(compare > 0) {
             throw new RequestException(ErrorCode.QR_EXPIRED_AT_BAD_REQUEST_400);
-        }
-
-        Optional<Member> member = memberRepository.findByUserAuthDetailId(userAuthDetailData.getId());
-
-        if(member.isEmpty()) {
-            throw new RequestException(ErrorCode.USER_NOT_FOUND_404);
         }
 
         return member.get();
