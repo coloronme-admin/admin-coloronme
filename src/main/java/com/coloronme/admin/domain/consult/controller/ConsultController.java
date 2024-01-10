086214df2127a81c633f6dd361b44ceb9ba4da99
TOG B <4ir@BTOG-MacBook-Pro.local>
2024-01-10 10:17:19 +0900

    🍕Chore : Consult API 수정

--- src/main/java/com/coloronme/admin/domain/consult/controller/ConsultController.java
+++ src/main/java/com/coloronme/admin/domain/consult/controller/ConsultController.java
@@ -40,34 +40,11 @@ public class ConsultController {
     private final UserAuthDetailService userAuthDetailService;
     private final JwtUtil jwtUtil;
 
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-    static {
-        // Java 8의 날짜 및 시간 유형을 처리하는 모듈 등록
-        objectMapper.registerModule(new JavaTimeModule());
-    }
-
-    /*Dto to Json*/
-    public static String convertDtoToJsonString(ConsultRequestDto consultRequestDto) {
-        try {
-            // ObjectMapper를 사용하여 객체를 JSON 문자열로 변환
-            return objectMapper.writeValueAsString(consultRequestDto);
-        } catch (JsonProcessingException e) {
-            // 예외 처리: JSON 변환 중에 오류가 발생한 경우
-            e.printStackTrace();
-            return null;
-        }
-    }
-
     @Operation(summary = "고객 상담 등록", description = "고객 상담 등록")
     @Transactional
     @PostMapping("/{userId}")
     public ResponseDto<ConsultUserResponseDto> registerConsultUser(HttpServletRequest request, @PathVariable int userId,
                                                                @Valid @RequestBody ConsultRequestDto consultRequestDto) throws IOException {
-        System.out.println("ConsultRequestDto : " + consultRequestDto);
-        System.out.println("getPersonalColorId : " + consultRequestDto.getPersonalColorId());
-        System.out.println("getConsultedDate : " + consultRequestDto.getConsultedDate());
-        System.out.println("getConsultedContent : " + consultRequestDto.getConsultedContent());
-
         int consultantId = jwtUtil.getIdFromRequest(request, "Access");
         ConsultUserResponseDto consultUserResponseDto = consultService.registerConsultUser(consultantId, userId, consultRequestDto);
         return ResponseDto.status(consultUserResponseDto);
@@ -95,18 +72,9 @@ public class ConsultController {
     @PatchMapping("/{userId}")
     public ResponseDto<ConsultUserResponseDto> updateConsultUser(HttpServletRequest request, @PathVariable int userId,
                                                  @Valid @RequestBody ConsultRequestDto consultRequestDto) {
-
-        System.out.println("ConsultRequestDto" + consultRequestDto);
-        System.out.println("getPersonalColorId" + consultRequestDto.getPersonalColorId());
-        System.out.println("getConsultedDate" + consultRequestDto.getConsultedDate());
-        System.out.println("getConsultedContent" + consultRequestDto.getConsultedContent());
-
-
-
         int consultantId = jwtUtil.getIdFromRequest(request, "Access");
         ConsultUserResponseDto consultUserResponseDto = consultService.updateConsultUser(consultantId, userId, consultRequestDto);
         return ResponseDto.status(consultUserResponseDto);
-       
     }
 
     @Operation(summary = "QR 고객 확인", description = "QR 고객 확인")
@@ -117,5 +85,4 @@ public class ConsultController {
         ConsultUserResponseDto consultUserResponseDto = consultService.verifyUserQr(consultantId, member);
         return ResponseDto.status(consultUserResponseDto);
     }
-
 }
