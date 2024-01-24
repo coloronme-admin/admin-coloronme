54246ef67ab1f8dc843e74ca1e0abaac5f78eb9e
erinlee <gusdk1793@gmail.com>
2024-01-24 17:20:55 +0900

    ⭐Feat[#65]:요일 및 시간대 차트 조회

--- src/main/java/com/coloronme/admin/domain/mainpage/dto/response/IntervalChartResponseDto.java
+++ src/main/java/com/coloronme/admin/domain/mainpage/dto/response/IntervalChartResponseDto.java
@@ -1,10 +1,34 @@
 package com.coloronme.admin.domain.mainpage.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class IntervalChartResponseDto {
     private String name;
-    private IntervalChartData intervalChartData;
+    private List<IntervalChartData> data = new LinkedList<>();
 
-    static class IntervalChartData {
+    public IntervalChartResponseDto(int hour) {
+        for(int i=0 ; i<hour ; i++) {
+            IntervalChartData intervalChartData = new IntervalChartData();
+            intervalChartData.setTime(i);
+            this.data.add(intervalChartData);
+        }
+    }
+
+    @Setter
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class IntervalChartData {
         private int time;
         private int count;
     }
--- src/main/java/com/coloronme/admin/domain/mainpage/service/MainPageService.java
+++ src/main/java/com/coloronme/admin/domain/mainpage/service/MainPageService.java
@@ -1,11 +1,15 @@
 package com.coloronme.admin.domain.mainpage.service;
 
+import com.coloronme.admin.domain.consult.entity.Consult;
 import com.coloronme.admin.domain.consult.repository.ConsultRepository;
 import com.coloronme.admin.domain.mainpage.dto.request.MainPageRequestDto;
 import com.coloronme.admin.domain.mainpage.dto.response.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,68 +18,110 @@ import java.util.List;
 public class MainPageService {
 
     private final ConsultRepository consultRepository;
+    private final int WEEK = 7; /*월, 화, 수, 목, 금, 토, 일*/
+    private final int HOUR = 24;
 
     /*퍼스널 컬러*/
     public List<ColorChartResponseDto> getUserDateByColor(int consultantId, MainPageRequestDto mainPageRequestDto) {
 
-        /*DB에서 데이터 추출*/
-        List<Object[]> resultList = consultRepository.getUserDataByColor(consultantId, mainPageRequestDto.getFrom(), mainPageRequestDto.getTo());
+        List<ColorChartResponseDto> colorChartList = null;
 
-        /*추출한 데이터를 DTO에 담기*/
-        List<ColorChartResponseDto> colorChartList = resultList.stream()
-                .map(result -> new ColorChartResponseDto(
-                        (String) result[0],
-                        Integer.parseInt(String.valueOf(result[1])),
-                        Double.parseDouble(String.valueOf(result[2]))
-                ))
-                .toList();
+        if(consultRepository.getUserDataCountByDate(consultantId, mainPageRequestDto.getFrom(), mainPageRequestDto.getTo())!=0) {
+            /*DB에서 데이터 추출*/
+            List<Object[]> resultList = consultRepository.getUserDataByColor(consultantId, mainPageRequestDto.getFrom(), mainPageRequestDto.getTo());
 
-        int limit = mainPageRequestDto.getTop();
+            /*추출한 데이터를 DTO에 담기*/
+            colorChartList = resultList.stream()
+                    .map(result -> new ColorChartResponseDto(
+                            (String) result[0],
+                            Integer.parseInt(String.valueOf(result[1])),
+                            Double.parseDouble(String.valueOf(result[2]))
+                    ))
+                    .toList();
 
-        /*정렬 수보다 많으면 나머지 데이터는 ETC로 만듦*/
-        if(colorChartList.size() > limit) {
+            int limit = mainPageRequestDto.getTop();
 
-            List<ColorChartResponseDto> colorChartResultList = new LinkedList<>();
+            /*정렬 수보다 많으면 나머지 데이터는 ETC로 만듦*/
+            if (colorChartList.size() > limit) {
 
-            int count = 0;
-            double percentage = 0;
+                List<ColorChartResponseDto> colorChartResultList = new LinkedList<>();
 
-            for (int i = 0; i < colorChartList.size(); i++) {
-                if(i>=limit) {
-                    count += colorChartList.get(i).getCount();
-                    percentage += colorChartList.get(i).getPercentage();
-                } else {
-                    colorChartResultList.add(colorChartList.get(i));
+                int count = 0;
+                double percentage = 0;
+
+                for (int i = 0; i < colorChartList.size(); i++) {
+                    if (i >= limit) {
+                        count += colorChartList.get(i).getCount();
+                        percentage += colorChartList.get(i).getPercentage();
+                    } else {
+                        colorChartResultList.add(colorChartList.get(i));
+                    }
                 }
+
+                ColorChartResponseDto colorChartResponseDto = new ColorChartResponseDto();
+                colorChartResponseDto.setName("ETC");
+                colorChartResponseDto.setCount(count);
+                colorChartResponseDto.setPercentage(percentage);
+
+                colorChartResultList.add(colorChartResponseDto);
+
+                return colorChartResultList;
             }
-
-            ColorChartResponseDto colorChartResponseDto = new ColorChartResponseDto();
-            colorChartResponseDto.setName("ETC");
-            colorChartResponseDto.setCount(count);
-            colorChartResponseDto.setPercentage(percentage);
-
-            colorChartResultList.add(colorChartResponseDto);
-
-            return colorChartResultList;
         }
         return colorChartList;
     }
 
     /*요일 및 시간대*/
-    public IntervalChartResponseDto getUserDataByInterval(int consultantId, MainPageRequestDto mainPageRequestDto) {
+    public List<IntervalChartResponseDto> getUserDataByInterval(int consultantId, MainPageRequestDto mainPageRequestDto) {
+
+        List<Consult> consultListByDate = consultRepository.getUserDataByDate(consultantId, mainPageRequestDto.getFrom(), mainPageRequestDto.getTo());
+
+        if(!consultListByDate.isEmpty()) {
+            List<IntervalChartResponseDto> intervalChartResultList = new LinkedList<>();
+
+            DayOfWeek day = DayOfWeek.MONDAY;
+
+            /*미리 MONDAY,, 요일 객체, 시간대 객체 설정*/
+            for(int i=0 ; i<WEEK ; i++) {
+                IntervalChartResponseDto intervalChartResponseDto = new IntervalChartResponseDto(HOUR);
+                intervalChartResponseDto.setName(day.plus(i).toString());
+                intervalChartResultList.add(intervalChartResponseDto);
+            }
+
+            for(int i=0 ; i<consultListByDate.size() ;i++ ){
+                Consult consult = consultListByDate.get(i);
+                LocalDateTime consultedDate = consult.getConsultedDate();
+                switch(consultedDate.getDayOfWeek().toString()) {
+                    case "MONDAY" -> {
+                        List<IntervalChartResponseDto.IntervalChartData> intervalChartDataList = intervalChartResultList.get(0).getData();
+
+                        IntervalChartResponseDto.IntervalChartData intervalChartData = new IntervalChartResponseDto.IntervalChartData();
 
 
+                    }  case "TUESDAY" -> {
+                        List<IntervalChartResponseDto.IntervalChartData> intervalChartDataList = intervalChartResultList.get(1).getData();;
 
+                    }  case "WEDNESDAY" -> {
+                        List<IntervalChartResponseDto.IntervalChartData> intervalChartDataList = intervalChartResultList.get(2).getData();;
 
+                    }  case "THURSDAY" -> {
+                        List<IntervalChartResponseDto.IntervalChartData> intervalChartDataList = intervalChartResultList.get(3).getData();;
 
+                    }  case "FRIDAY" -> {
+                        List<IntervalChartResponseDto.IntervalChartData> intervalChartDataList = intervalChartResultList.get(4).getData();;
 
+                    }  case "SATURDAY" -> {
+                        List<IntervalChartResponseDto.IntervalChartData> intervalChartDataList = intervalChartResultList.get(5).getData();;
 
+                    }  case "SUNDAY" -> {
+                        List<IntervalChartResponseDto.IntervalChartData> intervalChartDataList = intervalChartResultList.get(6).getData();;
 
+                    }
+                }
 
-
-
-
-
+                intervalChartResultList.add(intervalChartResponseDto);
+            }
+        }
         return null;
     }
 
