ffd009f8af0387331dbe25085d3b1ad382423993
erinlee <gusdk1793@gmail.com>
2024-01-24 15:17:12 +0900

    ⭐Feat[#64] : 퍼스널 컬러 차트 조회 추가

--- src/main/java/com/coloronme/admin/domain/mainpage/dto/response/IntervalChartResponseDto.java
+++ src/main/java/com/coloronme/admin/domain/mainpage/dto/response/IntervalChartResponseDto.java
@@ -1,4 +1,11 @@
 package com.coloronme.admin.domain.mainpage.dto.response;
 
 public class IntervalChartResponseDto {
+    private String name;
+    private IntervalChartData intervalChartData;
+
+    static class IntervalChartData {
+        private int time;
+        private int count;
+    }
 }
